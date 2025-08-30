396fe6384ae241982d851e304d4aeb3585413fbd
nourelhadidy <nourelhadidy183@gmail.com>
2025-08-30 03:08:42 +0300

    Update nav.component.ts

--- booking-frontend/src/app/components/nav/nav.component.ts
+++ booking-frontend/src/app/components/nav/nav.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  menuOpen: boolean = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
